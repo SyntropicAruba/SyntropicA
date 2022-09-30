2e0c2f4210f3723e7a60553d6f85660d2a80bb1a
Peter Curet <petercuret@gmail.com>
2022-09-30 19:32:18 -0400

    Trying akamai for images

--- next.config.js
+++ next.config.js
@@ -11,5 +11,10 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
   // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',  
 })
 module.exports = withNextra()
