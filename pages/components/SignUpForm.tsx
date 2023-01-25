96d12eb397faf484bd3da40f90a82b0304a379d1
Peter Curet <petercuret@gmail.com>
2023-01-25 15:17:13 -0400

    Fixes image loading

--- pages/components/SignUpForm.tsx
+++ pages/components/SignUpForm.tsx
@@ -18,7 +18,7 @@ const SignUpForm = ({ email }: { email: string }) => {
         <section id="contact" className="bg-gray-50 dark:bg-gray-900 dark:bg-gray-800 text-gray-800">
             <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
                 <div className="mx-auto max-w-screen-sm">
-                    <Image src="/illustration.png" width="900" height="568" className="w-full rounded-lg" />
+                    <Image src="/Illustration.png" width="900" height="568" className="w-full rounded-lg" />
                     <h2 className="text-center mb-2 mt-1 text-4xl font-extrabold leading-tight text-gray-900 dark:text-white">Sign up for a Food Forest</h2>
                     <div className="text-center mb-6 max-w-2xl font-light text-gray-600 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Our community will be happy to help you! </div>
                     <form action={`https://formsubmit.co/${email}`} method="POST" autoComplete="off">
